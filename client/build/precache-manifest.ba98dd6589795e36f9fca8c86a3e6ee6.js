daf10527ee2bc7c03c909db6cc4de26ab58321e5
Levike34 <89289513+Levike34@users.noreply.github.com>
2021-10-03 02:05:00 +0900

    Add files via upload

---
+++ client/build/precache-manifest.ba98dd6589795e36f9fca8c86a3e6ee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82916effcac0bb65c7048a4493c511b",
+    "url": "/tokenswap/index.html"
+  },
+  {
+    "revision": "c5670e9cec6e3fac0942",
+    "url": "/tokenswap/static/css/main.47ec8507.chunk.css"
+  },
+  {
+    "revision": "cd6d5c7c74174593e946",
+    "url": "/tokenswap/static/js/2.f809a70d.chunk.js"
+  },
+  {
+    "revision": "c5670e9cec6e3fac0942",
+    "url": "/tokenswap/static/js/main.252c75e7.chunk.js"
+  },
+  {
+    "revision": "09b2d31f87531ab00e4b",
+    "url": "/tokenswap/static/js/runtime-main.d27333c9.js"
+  }
+]);
